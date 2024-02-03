06ff396a2f57303081e0ec960114de1908141d41
Stefan Lazariotis <stefan@shopnative.io>
2024-02-03 14:56:23 +0100

    updated text

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         Welcome to the Random Bunny distributor. This site is for people who
         have too many bunnies or are eager to receive a bunny. Why are you here?
         Choose - but choose wiesly. There is no turning back (except via the
-        back button I guess). Hush now.
+        back button I guess). Hush now or no bunny for you.
       </p>
       <Button href={'/donate-bunny'} type="default">
         Donate a bunny
