1726b185a8746b6cb5e530b11616e8b701b06231
Stefan Lazariotis <stefan@shopnative.io>
2024-02-21 19:01:45 +0100

    added bunny prisma service

---
+++ backend/src/modules/prisma/bunny.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Bunny, Prisma } from '@prisma/client';
+
+@Injectable()
+export class BunnyService {
+  constructor(private prisma: PrismaService) {}
+
+  async bunny(
+    bunnyWhereUniqueInput: Prisma.BunnyWhereUniqueInput,
+  ): Promise<Bunny | null> {
+    return this.prisma.bunny.findUnique({
+      where: bunnyWhereUniqueInput,
+    });
+  }
+
+  async createBunny(data: Prisma.BunnyCreateInput): Promise<Bunny> {
+    return this.prisma.bunny.create({
+      data: {
+        name: data.name,
+        description: data.description,
+        email: data.email,
+      },
+    });
+  }
+
+  async deleteBunny(where: Prisma.BunnyWhereUniqueInput): Promise<Bunny> {
+    return this.prisma.bunny.delete({
+      where,
+    });
+  }
+}
